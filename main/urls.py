7e7afbbe3773f26f8e5f75c03feb3b6b80206821
NesreenMassimi <nmosimi@gmail.com>
2019-04-03 11:30:18 +0300

    mapping urls

--- main/urls.py
+++ main/urls.py
@@ -1,20 +1,20 @@
 from main import views
-from django.urls import path, include
+from django.urls import path
 from .views import UserListview
 
 users = UserListview.as_view({
     'get': 'list',
     'post': 'create'
 })
-'''regester = Creteviewset.as_view(
- {
-    'post':'createnew'
 
-  })'''
 user_details = UserListview.as_view(
- {  
-     'get' : 'retrieve'
-})
+ {
+     'get' : 'retrieve',
+     'delete': 'destroy'
+
+ })
+
+
 
 urlpatterns = [path('Users/', users,name='users'),
                path('Users/<int:pk>/',user_details,name='user_details')]
