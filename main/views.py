82c61e152ea11a77c65d3bbbeaeaeadfe030d3e6
NesreenMassimi <nmosimi@gmail.com>
2019-04-03 11:30:43 +0300

    views classes

--- main/views.py
+++ main/views.py
@@ -3,10 +3,11 @@ from rest_framework.response import Response
 from main.models import User
 from main.serializer import UserSerializer
 from rest_framework import viewsets
+from django.http import Http404
+
 
 
 # Create your views here.
- 
 
 class UserListview(viewsets.ModelViewSet):
 	queryset = User.objects.all()
@@ -24,4 +25,19 @@ class UserListview(viewsets.ModelViewSet):
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	def destroy(self, request, *args, **kwargs):
+		try:
+			instance = self.get_object(*args)
+			self.perform_destroy(instance)
+			instance.delete()
+		except Http404:
+			pass
+		return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
 
